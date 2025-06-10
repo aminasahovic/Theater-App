32a1504a0359eb2d2aa1a6497f624460a24a8bb0
aminasahovic <asahovic01@gmail.com>
2025-06-10 20:00:30 +0200

    Add QR code scanner

--- eTheater/UI/etheater_mobile/lib/screens/sala_kontrola_screen.dart
+++ eTheater/UI/etheater_mobile/lib/screens/sala_kontrola_screen.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
 
-class SalaKontrolaScreen extends StatelessWidget {
+class SalaKontrolaScreen extends StatefulWidget {
   const SalaKontrolaScreen({super.key});
+
+  @override
+  State<SalaKontrolaScreen> createState() => _SalaKontrolaScreenState();
+}
+
+class _SalaKontrolaScreenState extends State<SalaKontrolaScreen> {
+  String _scanResult = 'Nema rezultata';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Kontrola ulaza i popunjenosti sale')),
-      body: const Center(
-        child: Text(
-          'Ovdje ide funkcionalnost za skeniranje karata i praćenje sale.',
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: QRCodeDartScanView(
+              scanInvertedQRCode: true,
+              typeScan: TypeScan.live,
+              onCapture: (result) {
+                setState(() {
+                  _scanResult = result.text ?? 'Nepoznat sadržaj';
+                });
+              },
+              onCameraError: (error) {
+                debugPrint('Greška kamere: $error');
+              },
+              imageDecodeOrientation: ImageDecodeOrientation.portrait,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text(
+                'Rezultat: $_scanResult',
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
